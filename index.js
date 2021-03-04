f4507ba2e83f3e666582073c6109e094604e72bf
TravisSLee <travisslee223@gmail.com>
2021-03-04 18:13:58 -0500

    Done.

---
+++ index.js
@@ -0,0 +1,14 @@
+let divide = function() {
+    let product = 2000 / 100
+    return product
+}
+
+let square = (num) => {
+    let product = num * num
+    return product
+}
+
+let add = (num1, num2) => {
+    let sum = num1 + num2
+    return sum
+}
